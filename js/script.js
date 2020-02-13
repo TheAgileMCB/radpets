8e77513a0a6c263be73ee966e2f9c58e0ec71585
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-02-13 16:46:47 -0600

    JavaScript scaffolding to take user input

--- js/script.js
+++ js/script.js
@@ -1,17 +1,73 @@
-var form = document.querySelector("form");
-var log = document.querySelector("#log");
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
 
-form.addEventListener("submit", function(event) {
-  var data = new FormData(form);
-  var output = "";
-  for (const entry of data) {
-    output = output + entry[0] + "=" + entry[1] + "\r";
-  };
-  log.innerText = output;
-  event.preventDefault();
-}, false);
+if (hourNow > 18) {
+    greeting = 'Good evening!';
+} else if (hourNow > 12) {
+    greeting = 'Good afternoon!';
+} else if (hourNow > 0) {
+    greeting = 'Good morning!';
+} else {
+    greeting = 'Welcome!';
+}
+
+document.getElementById("greet").innerHTML = greeting;
+
+var howMany = function() {
+    var count = prompt('how many living things do you want to order?');
+  
+    while (count === '' || isNaN(count)) {
+      count = prompt('please enter a number');
+    }
+  
+    console.log('howMany():', Number(count));
+    return Number(count);
+  }
+  
+  var getItem = function() {
+    var order = prompt('what do you want to order?').toLowerCase;
+    var item;
+  
+    while (order !== 'dogs' && order !== 'cats' && order !== 'bunnies' && order !== 'people') {
+      order = prompt('please enter dogs, cats, bunnies, or people');
+    }
+  
+    if (order === 'cats') {
+        document.getElementById("cat").className = "selected"    
+    } else if (order === 'dogs') {
+        document.getElementById("dog").className = "selected"    
+    } else if (order === 'bunnies') {
+        document.getElementById("bunny").className = "selected"
+    } else if (order === 'people') {
+        window.open("https://www.match.com");
+}
+  
+    console.log('getItem():', item);
+  
+    document.write(item);
+    return item;
+  }
+  
+  var showOrder = function() {
+    var item = getItem();
+    var total = howMany();
+    var result = '';
+  
+    for (var i = 0; i < total; i++) {
+      result = result + '<p>Model #' + i + ' ' + item + '</p>';
+    }
+  
+    console.log('showOrder():', result);
+    return result;
+  }
+  
+  // howMany();
+  // getItem();
+  showOrder();
+
 
 function printToday(){
-    var today = new Date();
+    // var today = new Date();
     document.write(today.toDateString())
 };
